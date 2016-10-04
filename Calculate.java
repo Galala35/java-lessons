e0d4e0e82d239f1a8e3dc7fa04efff24bb5822d7
Galala35 <C:\Documents and Settings\\Application Data\The Bat!>
2016-10-04 14:14:08 +0400

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,8 +1,12 @@
 public class Calculate {
 
 	public static void main (String[] arg) {
-		System.out.println("Calculate...");	
-
+		System.out.println("Calculate...");
+		int first, second, sum;	
+		first = Integer.valueOf(arg[0]);
+		second = Integer.valueOf(arg[1]);
+		sum = first + second;
+		System.out.println("Sum="+sum);
 }
 
 }
